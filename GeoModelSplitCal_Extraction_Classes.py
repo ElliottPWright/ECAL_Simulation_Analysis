91c3ef1e2f7f7db0aff5ffd9b22a4d0b9a3897de
ElliottPWright <elliottpwright@gmail.com>
2026-07-31 14:07:06 +0200

    31.07 commit

--- GeoModelSplitCal_Extraction_Classes.py
+++ GeoModelSplitCal_Extraction_Classes.py
@@ -23,7 +23,6 @@ class SimFileReader:
         trees = self.list_trees()
         tree = self.file[trees[0]]  # Assuming the first tree is the one we want
         df = tree.arrays(branches, library="ak")
-
         data = [df[branch] for branch in branches]
 
         return data 
@@ -34,35 +33,98 @@ class SimFileReader:
             Sim = SimFileReader(os.path.join(file_path, "sim_mu-_10000MeV_2160mm.root"))
             
             branches = ["edep", "x_global", "y_global", "z_global", "layer"]
+
             edeps, x_globals, y_globals, z_globals, layers = Sim.get_branches(branches)
 
-            # Now concatenate all events using awkward
+            # Now mask the arrays so that the relevant plots can be made
 
-            # edeps = ak.to_numpy(ak.flatten(edeps))
-            # x_globals = ak.to_numpy(ak.flatten(x_globals))
-            # y_globals = ak.to_numpy(ak.flatten(y_globals))
-            # z_globals = ak.to_numpy(ak.flatten(z_globals))
-            # layers = ak.to_numpy(ak.flatten(layers))
+            first_layer_edeps = edeps[:][layers == 1]
+            last_layer_edeps = edeps[:][layers == np.max(layers)]
 
-            first_layer_edeps = [edeps[i][layers[i] == 1] for i in range(len(edeps))]
-            last_layer_edeps = [edeps[i][layers[i]==np.max(layers[i])] for i in range(len(edeps))]
+            first_layer_edeps = ak.flatten(first_layer_edeps)
+            last_layer_edeps = ak.flatten(last_layer_edeps)
 
-            ## First layer energy histogram 
+            first_layer_x_hits = x_globals[:][layers == 1]
+            last_layer_x_hits = x_globals[:][layers == np.max(layers)]
+            first_layer_x_hits = ak.to_numpy(ak.flatten(first_layer_x_hits))
+            last_layer_x_hits = ak.to_numpy(ak.flatten(last_layer_x_hits))
+            
+            first_layer_y_hits = y_globals[:][layers == 1]
+            last_layer_y_hits = y_globals[:][layers == np.max(layers)]
+            first_layer_y_hits = ak.to_numpy(ak.flatten(first_layer_y_hits))
+            last_layer_y_hits = ak.to_numpy(ak.flatten(last_layer_y_hits))
+
+            acceptance_energy = np.sum(last_layer_edeps)/np.sum(first_layer_edeps)
+
+            # All layers energy histogram
+            with plt.style.context(['science', 'no-latex']):
+                plt.rcParams['figure.dpi'] = 200
+                plt.hist(ak.to_numpy(ak.flatten(edeps)), bins = 100)
+                plt.xlabel("Energy [MeV]")
+                plt.ylabel("Counts")
+                plt.yscale("log")
+                #plt.savefig(os.path.join(plotting_directory, "SplitCalGap_first_layer_energy.png"))
+                plt.show()
+            
+
+            # First layer energy histogram 
             with plt.style.context(['science', 'no-latex']):
                 plt.rcParams['figure.dpi'] = 200
                 plt.hist(first_layer_edeps, bins = 100)
-                plt.xlabel("Energy [GeV]")
+                plt.xlabel("Energy [MeV]")
                 plt.ylabel("Counts")
+                plt.yscale("log")
                 #plt.savefig(os.path.join(plotting_directory, "SplitCalGap_first_layer_energy.png"))
                 plt.show()
 
+            # Last layer energy histogram 
             with plt.style.context(['science', 'no-latex']):
                 plt.rcParams['figure.dpi'] = 200
                 plt.hist(last_layer_edeps, bins = 100)
-                plt.xlabel("Energy [GeV]")
+                plt.xlabel("Energy [MeV]")
                 plt.ylabel("Counts")
+                plt.yscale("log")
                 #plt.savefig(os.path.join(plotting_directory, "SplitCalGap_final_layer_energy.png"))
                 plt.show()
+
+            # All layers xy-hits histogram 
+            with plt.style.context(['science', 'no-latex']):
+                plt.rcParams['figure.dpi'] = 200
+                plt.hist2d(ak.to_numpy(ak.flatten(x_globals)), ak.to_numpy(ak.flatten(y_globals)), bins = 100, cmap ='viridis', cmin =1)
+                plt.xlabel("x [mm]")
+                plt.ylabel("y [mm]")
+                plt.colorbar(label = 'Counts')
+                #plt.savefig(os.path.join(plotting_directory, "SplitCalGap_final_layer_energy.png"))
+                plt.show()
+
+            # First xy-hits histogram 
+            with plt.style.context(['science', 'no-latex']):
+                plt.rcParams['figure.dpi'] = 200
+                plt.hist2d(first_layer_x_hits, first_layer_y_hits, bins = 100, cmap ='viridis', cmin =1)
+                plt.xlabel("x [mm]")
+                plt.ylabel("y [mm]")
+                plt.colorbar(label = 'Counts')
+                #plt.savefig(os.path.join(plotting_directory, "SplitCalGap_first_layer_energy.png"))
+                plt.show() 
+
+            # First layer xy-hits histogram 
+            with plt.style.context(['science', 'no-latex']):
+                plt.rcParams['figure.dpi'] = 200
+                plt.hist2d(last_layer_x_hits, last_layer_y_hits, bins = 100, cmap ='viridis', cmin =1)
+                plt.xlabel("x [mm]")
+                plt.ylabel("y [mm]")
+                plt.colorbar(label = 'Counts')
+                #plt.savefig(os.path.join(plotting_directory, "SplitCalGap_first_layer_energy.png"))
+                plt.show()
+            
+            # Acceptance energy-based
+            with plt.style.context(['science', 'no-latex']):
+                plt.rcParams['figure.dpi'] = 200
+                plt.plot(2160, acceptance_energy)
+                plt.xlabel("Crack size [mm]")
+                plt.ylabel("Acceptance")
+                #plt.savefig(os.path.join(plotting_directory, "SplitCalGap_first_layer_energy.png"))
+                plt.show() 
 
     def close(self):
         self.file.close()
